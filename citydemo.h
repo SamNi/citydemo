44cbfb29f2678800d0771d3a248df371b32f44b6
SamNi <sgnieto@uci.edu>
2014-08-13 19:43:50 -0700

    ditto, also removed references to culled modules

--- citydemo.h
+++ citydemo.h
@@ -1,14 +1,11 @@
 // Copyright [year] <Copyright Owner>
 #include "essentials.h"
-#include "World.h"
-#include "Renderer.h"
 
 #include "GL.H"
 #include "GLM.h"
 #include "Shader.h"
 #include "Texture.h"
 #include "ParticleSystem.h"
-#include "Contouring.h"
 #include "Backend.h"
 #include "ResourceManager.h"
-#include "Application.h"
+#include "Application.h" 
