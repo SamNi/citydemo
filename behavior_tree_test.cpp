ae007a1eceb8dcac67f6f493a397910ac2cc2ffe
SamNi <sgnieto@uci.edu>
2014-08-15 02:40:37 -0700

    forgot to test the selector as well, oops

--- behavior_tree_test.cpp
+++ behavior_tree_test.cpp
@@ -10,13 +10,26 @@ struct MyAction : public BTAction {
     }
 };
 
+struct MyCondition : public BTAction {
+    explicit MyCondition(void) { }
+    virtual BTStatus execute(void) {
+        static int n = 1;
+        LOG(LOG_TRACE, "MyCondition %08X with n = %d", this, n);
+        return (n++ == 5) ? BT_FAILURE : BT_SUCCESS;
+    }
+};
+
 int main(void) {
     {
         BTSequence sequencer;
-
         sequencer.m_children.push_back(BTPtr(new MyAction()));
-
         while (BT_SUCCESS == sequencer.execute());
+    }
+
+    {
+        BTSelector selector;
+        selector.m_children.push_back(BTPtr(new MyCondition()));
+        while (BT_SUCCESS == selector.execute());
     }
     return 0;
 }
