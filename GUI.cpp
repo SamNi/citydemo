d8bed115d423ec2ed444c1a1f454beb8dacd01cb
SamNi <sgnieto@uci.edu>
2014-08-16 03:20:02 -0700

    Preliminary GUI framework

---
+++ GUI.cpp
@@ -0,0 +1,70 @@
+#include "GL.h"
+#include "GUI.h"
+
+namespace GUI {
+
+// a lot of these definitions will be blank
+// this is intentional
+
+static uint16_t unique_id_counter = 0;
+
+Widget::Widget(void) : m_visible(false) {
+    m_bounds.minor_extent = glm::vec2(0.0f, 0.0f);
+    m_bounds.major_extent = glm::vec2(1.0f, 1.0f);
+    m_unique_id = unique_id_counter++;
+}
+
+Widget::~Widget(void) { 
+    //for (auto child : m_children)
+        //child.reset();
+}
+
+void Widget::show(void) { 
+    m_visible = true;
+    for (auto child : m_children)
+        child->show();
+}
+void Widget::hide(void) { 
+    m_visible = false;
+    for (auto child : m_children)
+        child->hide();
+}
+void Widget::draw(void) const {
+    if (false == m_visible)
+        return;
+
+    // Draw yourself first, then the children
+    // ....
+    for (auto child : m_children)
+        child->draw();
+}
+
+void Widget::add_child_widget(WidgetPtr w) {
+    m_children.push_back(w);
+}
+
+WidgetManager::WidgetManager(void) :
+    m_root(nullptr),
+    m_focus(nullptr) {
+}
+WidgetManager::~WidgetManager(void) {
+    //m_root.reset();
+}
+
+void WidgetManager::set_root(WidgetPtr root) {
+    assert(nullptr != root);
+    m_root = root;
+}
+
+void WidgetManager::set_focus(WidgetPtr focus) {
+    assert(nullptr != focus);
+    m_focus = focus;
+}
+
+void WidgetManager::draw(void) const {
+    if (nullptr == m_root)
+        return;
+    m_root->draw();
+}
+
+}  // ~namespace GUI
---
+++ GUI.h
@@ -0,0 +1,75 @@
+#ifndef _GUI_H
+#define _GUI_H
+#include "essentials.h"
+
+/* 
+    Ground rules: This GUI strictly works in a virtual space
+    with dimensions ([0, A], [0, 1.0]) where A is the aspect
+    ratio of the display, which may vary. Floating point is
+    the order of the day, not unsigned integers of 1600, 900,
+    720, etc.
+
+    (0, 0) is the lower left corner.
+    (A, 1) is the upper right corner.
+
+    Trying to minimize, hopefully have absolutely no OpenGL
+    calls in my GUI module. It should be reliant upon the
+    frontend and just maybe, maybe the backend.
+
+    For this framework I have decided to go with instanced
+    rendering with per-instance uniforms for the modelview
+    matrices.
+*/
+
+namespace GUI {
+
+struct WidgetManager;
+struct Widget;
+struct AABB;
+
+typedef std::shared_ptr<Widget> WidgetPtr;
+typedef std::list<WidgetPtr> Container;
+typedef AABB AxisAlignedBoundingbox;
+typedef glm::uvec2 Vec2;
+
+struct AABB {
+    Vec2            minor_extent;
+    Vec2            major_extent;
+};
+
+struct Widget {
+    friend          WidgetManager;
+    explicit        Widget(void);
+    virtual         ~Widget(void);
+
+    virtual void    show(void);
+    virtual void    hide(void);
+
+    virtual void    add_child_widget(WidgetPtr w);
+
+protected:
+    virtual void    draw(void) const;
+
+    AABB            m_bounds;
+    Container       m_children;
+    bool            m_visible;
+    uint16_t        m_unique_id;
+};
+
+struct WidgetManager {
+    explicit        WidgetManager(void);
+    virtual         ~WidgetManager(void);
+
+    void            set_root(WidgetPtr w);
+    void            set_focus(WidgetPtr w);
+
+    virtual void    draw(void) const;
+
+protected:
+    WidgetPtr       m_root;
+    WidgetPtr       m_focus;
+};
+
+}  // ~namespace GUI
+
+#endif  // ~_GUI_H
