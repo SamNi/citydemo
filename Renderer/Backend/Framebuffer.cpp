6e792adf0e714840afb451963bb9aefb009afba0
SamNi <sgnieto@uci.edu>
2014-08-29 00:10:25 -0700

    oh hey it's the framebuffer manager I forgot to commit

--- Renderer/Backend/Framebuffer.cpp
+++ Renderer/Backend/Framebuffer.cpp
@@ -1,4 +1,17 @@
 #include "Backend_local.h"
+#include <unordered_map>
+
+struct Framebuffer {
+    explicit Framebuffer(uint16_t w, uint16_t h);
+    ~Framebuffer(void);
+    void bind(void) const;
+    void blit(void) const;
+    uint32_t get_texture_id(void) const;
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> m_impl;
+};
 
 struct Framebuffer::Impl {
     explicit Impl(uint16_t w, uint16_t h) : width(w), height(h) {
@@ -33,18 +46,17 @@ struct Framebuffer::Impl {
         checkGL();
     }
 
-    void blit(int w, int h) const {
+    void blit() const {
         static const auto identity_matrix = glm::mat4x4(1.0f);
 
         // switch to the visible render buffer
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
         glBindTexture(GL_TEXTURE_2D, textureID); // the texture we just drew to is now the source
-        glViewport(0, 0, w, h);
+        Backend::reset_viewport();
         Backend::set_modelview(identity_matrix);
         Backend::set_projection(identity_matrix);
-        glBindVertexArray(QuadVAO::get_vao());
         Backend::disable_depth_testing();
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+        Backend::draw_fullscreen_quad();
     }
     uint32_t          framebufferID;
     uint32_t          textureID;
@@ -55,13 +67,61 @@ struct Framebuffer::Impl {
 Framebuffer::Framebuffer(uint16_t w, uint16_t h) {
     m_impl = std::unique_ptr<Impl>(new Impl(w, h));
 }
-Framebuffer::~Framebuffer(void) {
-    m_impl.reset(nullptr);
-}
-void Framebuffer::bind(void) const {
-    m_impl->bind();
-}
-void Framebuffer::blit(int w, int h) const {
-    m_impl->blit(w, h);
-}
+Framebuffer::~Framebuffer(void) { m_impl.reset(nullptr); }
+void Framebuffer::bind(void) const { m_impl->bind(); }
+void Framebuffer::blit(void) const { m_impl->blit(); }
 uint32_t Framebuffer::get_texture_id(void) const { return m_impl->textureID; }
+
+
+// -------------------------------------------------------
+// FramebufferManager
+// -------------------------------------------------------
+
+struct FramebufferManager::Impl {
+    static uint32_t m_fb_counter;
+
+    typedef std::unique_ptr<Framebuffer> FramebufferPtr;
+    std::unordered_map<uint32_t, FramebufferPtr> m_framebuffer_collection;
+
+    uint32_t create(uint16_t w, uint16_t h) {
+        assert((w > 0) && (h > 0));
+        m_framebuffer_collection.emplace(m_fb_counter, FramebufferPtr(new Framebuffer(w, h)));
+        return m_fb_counter++;
+    }
+    void bind(uint32_t handle) {
+        auto it = m_framebuffer_collection.find(handle);
+        if (it == m_framebuffer_collection.end()) {
+            LOG(LOG_WARNING, "FramebufferManager::bind called on nonexistent handle %u", handle);
+            return;
+        }
+        it->second->bind();
+    }
+    void unbind(void) {
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    }
+    void blit(uint32_t handle) {
+        auto it = m_framebuffer_collection.find(handle);
+        if (it == m_framebuffer_collection.end()) {
+            LOG(LOG_WARNING, "FramebufferManager::blit called on nonexistent handle %u", handle);
+            return;
+        }
+        it->second->blit();
+#if 0
+        unbind();
+        Backend::bind_texture(it->second->get_texture_id());
+        Backend::set_modelview(glm::mat4(1.0f));
+        Backend::set_projection(glm::mat4(1.0f));
+        Backend::disable_depth_testing();
+        Backend::reset_viewport();
+        Backend::draw_fullscreen_quad();
+#endif
+    }
+};
+uint32_t FramebufferManager::Impl::m_fb_counter = 1;
+
+FramebufferManager::FramebufferManager(void) { m_impl = ImplPtr(new Impl()); }
+FramebufferManager::~FramebufferManager(void) { m_impl.reset(nullptr); }
+uint32_t FramebufferManager::create(uint16_t w, uint16_t h) { return m_impl->create(w, h); }
+void FramebufferManager::bind(uint32_t handle) { m_impl->bind(handle); }
+void FramebufferManager::unbind(void) { m_impl->unbind(); }
+void FramebufferManager::blit(uint32_t handle) { m_impl->blit(handle); }
